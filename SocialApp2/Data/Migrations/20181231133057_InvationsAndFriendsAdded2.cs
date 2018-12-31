c90e04787e0b419072d1066c3ad2457aa46f9224
varenghar <varenghar@users.noreply.github.com>
2018-12-31 13:31:53 +0000

    small bug fixed

---
+++ SocialApp2/Data/Migrations/20181231133057_InvationsAndFriendsAdded2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SocialApp2.Data.Migrations
+{
+    public partial class InvationsAndFriendsAdded2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ SocialApp2/InvitationsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SocialApp2.Data;
+using SocialApp2.Models;
+
+namespace SocialApp2
+{
+    public class InvitationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InvitationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Invitations
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Invitation.Include(i => i.Sender);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Invitations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invitation = await _context.Invitation
+                .Include(i => i.Sender)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (invitation == null)
+            {
+                return NotFound();
+            }
+
+            return View(invitation);
+        }
+
+        // GET: Invitations/Create
+        public IActionResult Create()
+        {
+            ViewData["SenderId"] = new SelectList(_context.Set<User>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Invitations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,SenderId")] Invitation invitation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(invitation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SenderId"] = new SelectList(_context.Set<User>(), "Id", "Id", invitation.SenderId);
+            return View(invitation);
+        }
+
+        // GET: Invitations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invitation = await _context.Invitation.FindAsync(id);
+            if (invitation == null)
+            {
+                return NotFound();
+            }
+            ViewData["SenderId"] = new SelectList(_context.Set<User>(), "Id", "Id", invitation.SenderId);
+            return View(invitation);
+        }
+
+        // POST: Invitations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,SenderId")] Invitation invitation)
+        {
+            if (id != invitation.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(invitation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InvitationExists(invitation.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SenderId"] = new SelectList(_context.Set<User>(), "Id", "Id", invitation.SenderId);
+            return View(invitation);
+        }
+
+        // GET: Invitations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invitation = await _context.Invitation
+                .Include(i => i.Sender)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (invitation == null)
+            {
+                return NotFound();
+            }
+
+            return View(invitation);
+        }
+
+        // POST: Invitations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var invitation = await _context.Invitation.FindAsync(id);
+            _context.Invitation.Remove(invitation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InvitationExists(int id)
+        {
+            return _context.Invitation.Any(e => e.Id == id);
+        }
+    }
+}
